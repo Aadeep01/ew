f13682f284d9858855b973c510397ca05825710f
Aadeep Kaur <aadeepkaur@yahoo.com>
2025-02-04 14:20:08 +0530

    updated

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+require("dotenv").config()
+
+const app = express()
+const port = 3000
+app.use(bodyParser.json())
+const mongo_uri='mongodb+srv://aadeepkaur:aTlas2607@cluster0.8by9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB:', err));
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
