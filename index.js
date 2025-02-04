91e361792663187bf758b81b2914b7a29d3b6d35
Aadeep Kaur <aadeepkaur@yahoo.com>
2025-02-04 14:26:14 +0530

    updated2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
 require("dotenv").config()
 
 const app = express()
